a2b125c620ef9bd82082bc1f2802832774d5fa88
Anthony Eden <anthony.louis.eden@gmail.com>
2017-08-25 14:19:10 -0400

    Revert "ethernet: remove dhcpcd service"
    This reverts commit d6d1ed1ff5de28eda704c4b26e03bd09b3d3910a.

--- ethernet/device.mk
+++ ethernet/device.mk
@@ -1,2 +1,5 @@
+PRODUCT_PACKAGES := ethernet.rc
+
 PRODUCT_COPY_FILES := \
 	frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+        $(LOCAL_PATH)/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
